83022db8e6db19bd3ed0e96f358c5bd1994a4f17
guillaumekordonian <guillaume.kordonian@gmail.com>
2018-03-21 16:34:20 +0100

    updated features on projects MVC

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def self.not_in_the_project(project)
+    self.all.select {|user| !user.projects.include? project}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :show, :destroy] do
     get "quickshow", to: "items#quickshow", as: :quickshow
   end
+  resources :user_projects, only: [ :destroy ]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
