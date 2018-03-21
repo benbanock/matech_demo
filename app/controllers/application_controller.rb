f4007022d43c6f2f87d21973c6767ac9213e1d6c
Benoit Morin <benoit.morin@hec.edu>
2018-03-21 19:41:19 +0100

    update of index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,9 +25,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
      # For additional fields in app/views/devise/registrations/new.html.erb
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :description, :company_name])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :description, :company_name, :avatar])
 
      # For additional in app/views/devise/registrations/edit.html.erb
-     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :description, :company_name])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :description, :company_name, :avatar])
   end
 end
