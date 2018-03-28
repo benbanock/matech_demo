465f730fc476ca8db2bc2249ddf11d71b36d9eb3
PaulaVandemeulebroucke <paula.vandemeulebroucke@gmail.com>
2018-03-28 12:49:02 +0200

    ok

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,10 @@ class Project < ApplicationRecord
   has_many :users, through: :user_projects
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, too_short: "please enter at least 3 characters"}
+
+
+#   def self.not_twice_in_item(item)
+#     self.all.select {|item| !item.projects.include? project}
+#   end
 end
+
